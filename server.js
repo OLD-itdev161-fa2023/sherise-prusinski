3278d0b71b7b36a4169682712b41b3c7a29fdebe
Sherise Prusinski <prusinsa@gmatc.matc.edu>
2022-10-06 21:33:08 -0500

    Browser - new users collection with test user

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ import express from 'express';
 import connectDatabase from './config/db';
 import { check, validationResult } from 'express-validator';
 import cors from 'cors';
+import bcrypt from 'bcryptjs';
+import user from './models/user';
+import User from './models/user';
 
 //Initialize express application
 const app = express();
@@ -10,7 +13,7 @@ const app = express();
 connectDatabase();
 
 //Configure Middleware
-app.use(express.json({extended : false}));
+app.use(express.json({ extended: false }));
 app.use(
     cors({
         origin: 'http://localhost:3000'
@@ -18,11 +21,11 @@ app.use(
 );
 
 //API endpoints
-app.get('/', (req,res) =>
-/**
- * @route GET /
- * @desc Test endpoint
- */
+app.get('/', (req, res) =>
+    /**
+     * @route GET /
+     * @desc Test endpoint
+     */
     res.send('https get request sent to root api endpoint')
 );
 
@@ -39,14 +42,40 @@ app.post(
         check('email', 'Please enter a valid email')
             .isEmail(),
         check('password', 'Please enter a password with 6 or more characters')
-            .isLength({min:6})
-    ],  
-    (req,res) => {
+            .isLength({ min: 6 })
+    ],
+    async (req, res) => {
         const errors = validationResult(req);
-        if(!errors.isEmpty()) {
-            return res.status(422).json({errors: errors.array()});
+        if (!errors.isEmpty()) {
+            return res.status(422).json({ errors: errors.array() });
         } else {
-            return res.send(req.body);
+            const { name, email, password } = req.body;
+            try {
+                //Check if user exists
+                let user = await User.findOne({ email: email });
+                if (user) {
+                    return res
+                        .status(400)
+                        .json({ errors: [{ msg: 'User already exists' }] });
+                }
+
+                //Create a new user
+                user = new User({
+                    name: name,
+                    email: email,
+                    password: password
+                });
+
+                //Encrypt the password
+                const salt = await bcrypt.genSalt(10);
+                user.password = await bcrypt.hash(password, salt);
+
+                //Save to the db and return
+                await user.save();
+                res.send('User successfully registered');
+            } catch (error) {
+                res.status(500).send('Server error');
+            }
         }
     }
 );
