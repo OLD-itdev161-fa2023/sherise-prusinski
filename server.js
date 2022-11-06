e0d06df3fc4d0364c6058ada59fc9425044193ed
Sherise Prusinski <prusinsa@gmatc.matc.edu>
2022-11-06 07:57:50 -0600

    Register User API, with returnToken function

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { json } from 'express';
 import connectDatabase from './config/db';
 import { check, validationResult } from 'express-validator';
 import cors from 'cors';
@@ -75,25 +75,8 @@ app.post(
                 //Save to the db and return
                 await user.save();
 
-                console.log("User info saved in database");
-
-                //Generate and return a JWT token
-                const payload = {
-                    user: {
-                        id: user.id
-                    }
-                };
-
-                jwt.sign(
-                    payload,
-                    config.get('jwtSecret'),
-                    { expiresIn: '10hr' },
-                    (err, token) => {
-                        if (err) throw err;
-                        res.json({ token: token });
-                    }
-                );
-
+                //Save to the db and return
+                returnToken(user, res);
             } catch (error) {
                 res.status(500).send('Server error : ' + error);
             }
@@ -113,6 +96,66 @@ app.get('/api/auth', auth, async (req, res) => {
         res.status(500).send('Unknown server error');
     }
 });
+
+/**
+ * @route POST api/login
+ * @desc Login user
+ */
+
+app.post(
+    'api/login',
+    [
+        check('email', 'Please enter a valid email').isEmail(),
+        check('password', 'A password is required').exists()
+    ],
+    async (req, res) => {
+        const errors = validatationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(422).json({ errors: errors.array() });
+        } else {
+            const { email, password } = req.body;
+            try {
+                //Check if user exists
+                let user = await User.findOne({ email: email });
+                if (!user) {
+                    return res
+                        .status(400)
+                        .json({ errors: [{ msg: 'Invalid email or password' }] });
+                }
+
+                //Check password
+                const match = await bcrypt.compare(password, user.password);
+                if (!match) {
+                    return res
+                        .status(400)
+                        .jason({ errors: [{ msg: 'Invalida email or password' }] });
+                }
+
+                returnToken(user, res);
+            } catch (error) {
+                res.status(500).send('Server error');
+            }
+        }
+    }
+);
+
+const returnToken = (user, res) => {
+    const payload = {
+        user: {
+            id: user.id
+        }
+    };
+
+    jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: '10hr' },
+        (err, token) => {
+            if (err) throw err;
+            res.json({ token: token });
+        }
+    );
+};
 
 //Connection listener
 const port = 5000;
