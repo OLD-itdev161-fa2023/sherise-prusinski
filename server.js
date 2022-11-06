3b314fcdb23cd0d43f33f98a5d94afc172feaedd
Sherise Prusinski <prusinsa@gmatc.matc.edu>
2022-11-06 15:21:16 -0600

    Get post endpoint

--- server.js
+++ server.js
@@ -114,6 +114,26 @@ app.get('/api/posts', auth, async (req, res) => {
 });
 
 /**
+ * @route GET api/post/:id
+ * @desc Get post
+ */
+app.get('/api/post:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        //Make sure the post was found
+        if (!post) {
+            return res.status(404).json({ msg: 'Post not found' });
+        }
+
+        res.json(post);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Server error : ' + error);
+    }
+});
+
+/**
  * @route POST api/login
  * @desc Login user
  */
