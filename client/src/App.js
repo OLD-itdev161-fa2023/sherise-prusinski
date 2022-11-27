1a9bfd40e77932fb176cd85793d93dbc8b0c17b7
Sherise Prusinski <prusinsa@gmatc.matc.edu>
2022-11-27 13:51:48 -0600

    Use the CreatePost and EditPost components

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,8 @@ import Register from './components/Register/Register';
 import Login from './components/Login/Login';
 import PostList from './components/PostList/PostList';
 import Post from './components/Post/Post';
+import CreatePost from './components/Post/CreatePost';
+import EditPost from './components/Post/EditPost';
 
 class App extends React.Component {
   state = {
@@ -119,6 +121,31 @@ class App extends React.Component {
     }
   };
 
+  editPost = post => {
+    this.setState({
+      post: post
+    });
+  };
+
+  onPostCreated = post => {
+    const newPosts = [...this.state.posts, post];
+
+    this.setState({
+      post: newPosts
+    });
+  };
+
+  onPostUpdated = post => {
+    console.log('updated post:', post);
+    const newPosts = [...this.state.posts, post];
+    const index = newPosts.findIndex(p => p._id === post._id);
+    newPosts[index] = post;
+
+    this.setState({
+      post: newPosts
+    });
+  };
+
   logOut = () => {
     localStorage.removeItem('token');
     localStorage.removeItem('user');
@@ -126,6 +153,7 @@ class App extends React.Component {
   }
 
   render() {
+    const token = localStorage.getItem('token');
     let { user, posts, post } = this.state;
     const authProps = {
       authenticateUser: this.authenticateUser
@@ -141,13 +169,18 @@ class App extends React.Component {
                 <Link to="/">Home</Link>
               </li>
               <li>
-                <Link to="/register">Register</Link>
+                {user ? (
+                  <Link to="/new-post">New Post</Link>
+                ) : (
+                  <Link to="/register">Register</Link>
+                )}
               </li>
               <li>
-                {user ?
-                  <Link to="" onClick={this.logOut}>Log out</Link> :
+                {user ? (
+                  <Link to="" onClick={this.logOut}>Log out</Link>
+                ) : (
                   <Link to="/login">Login</Link>
-                }
+                )}
               </li>
             </ul>
           </header>
@@ -161,6 +194,7 @@ class App extends React.Component {
                       posts={posts}
                       clickPost={this.viewPost}
                       deletePost={this.deletePost}
+                      editPost={this.editPost}
                     />
                   </React.Fragment>
                 ) : (
@@ -169,6 +203,16 @@ class App extends React.Component {
               </Route>
               <Route path="/posts/:postId" >
                 <Post post={post} />
+              </Route>
+              <Route path="/new-post">
+                <CreatePost token={token} onPostCreated={this.onPostCreated} />
+              </Route>
+              <Route path="/edit-post/:postId">
+                <EditPost
+                  token={token}
+                  post={post}
+                  onPostUpdated={this.onPostUpdated}
+                />
               </Route>
               <Route
                 exact
